612de19f0855697a7dcd6b39e0e9ceedcf464417
AlbertoParente <albertoparente20@gmail.com>
2021-07-02 23:44:16 -0300

    adding

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -12,12 +12,26 @@ const debtSchema = new mongoose.Schema({
     status: { type: String, required: false, uppercase: true, enum: ['Pay', 'Pending', 'Planned'] }
 })
 
+const covenantSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true }
+})
+
+const termOfPaymentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: [true, 'Enter the covenant amount!'] },
+    status: { type: String, required: false, uppercase: true, enum: ['Pay', 'Pending', 'Planned'] }
+})
+
 const billingCycleSchema = new mongoose.Schema({
     name: { type: String, required: true },
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
     credits: [creditSchema],
-    debts: [debtSchema]
+    debts: [debtSchema],
+    covenant: [covenantSchema],
+    termOfPayment: [termOfPaymentSchema]
+
 })
 
 module.exports = restful.model('BillingCycle', billingCycleSchema)
